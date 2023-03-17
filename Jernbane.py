aa75972e5cb91ae58520f78b433603140737ed3e
Sander Garland Slinning <sandergs@stud.ntnu.no>
2023-03-17 12:45:53 +0100

    Brukerhistorie E er ferdig.

--- Jernbane.py
+++ Jernbane.py
@@ -1,9 +1,10 @@
 import sqlite3
-
+con = sqlite3.connect('Jernbane.db')
+cursor = con.cursor()
 # Funksjon for brukerhistorie c)
+
+
 def getTogruter(stasjon, ukedag):
-    con = sqlite3.connect('Jernbane.db')
-    cursor = con.cursor()
     cursor.execute(
         """SELECT DagerTogruterKjører.RuteID FROM DagerTogruterKjører 
         INNER JOIN 'Stasjon på rute' ON DagerTogruterKjører.RuteID = 'Stasjon på rute'.RuteID 
@@ -16,13 +17,47 @@ def getTogruter(stasjon, ukedag):
     con.close()
     print(togruter)
 
+# Funksjon for brukerhistorie e)
+
+def addKunde(navn, epost, telefon):
+    con = sqlite3.connect('Jernbane.db')
+    cursor = con.cursor()
+    cursor.execute(
+        """INSERT INTO Kunde (Navn, 'E-postadresse', Mobilnummer) VALUES (:navn, :epost, :telefon)""",
+        {"navn": navn, "epost": epost, "telefon": telefon}
+    )
+    con.commit()
+    con.close()
+
 
 print("Hei! Velkommen til vårt Jernaneprogram")
 
-svar = input("Hva vil du gjøre? \n Skriv 'finn stasjon' for å få alle togruter som går på den oppgitte stasjonen på den oppgitte dagen.")
+svar = ""
 
-if svar == "finn stasjon":
-    stasjon = input("Hvilken stasjon vil du finne?")
-    dag = input("Hvilken dag vil du finne?")
-    print("Her er alle togruter som går på stasjonen", stasjon, "på", dag)
-    getTogruter(stasjon, dag)
+while svar != "avslutt":
+
+    svar = input("""Hva vil du gjøre? 
+            \n Skriv 'finn stasjon' for å få alle togruter som går på den oppgitte stasjonen på den oppgitte dagen.
+            \n Skriv 'registrer bruker' for å registrere en ny bruker. 
+            \n Skriv 'avslutt' for å avslutte programmet.\n""")
+
+    if svar == "finn stasjon":
+        stasjon = input("Hvilken stasjon vil du finne? ")
+        dag = input("Hvilken dag vil du finne? ")
+        print("Her er alle togruter som går på stasjonen", stasjon, "på", dag)
+        getTogruter(stasjon, dag)
+
+    elif svar == "registrer bruker":
+        navn = input("Skriv ditt fullstendige navn: ")
+        epost = input("Skriv din epost: ")
+        telefon = input("Skriv ditt telefonnummer: ")
+        addKunde(navn, epost, telefon)
+        print("Kunde registrert")
+        cursor.execute("""SELECT * FROM Kunde""")
+        print(cursor.fetchall())
+
+
+    elif svar == "avslutt":
+        print("Programmet avsluttes")
+        break
+
