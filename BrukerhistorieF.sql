931d1dc1dec4dad4ed0676c1727d38bdd627115d
unknown <bcfogale@stud.ntnu.no>
2023-03-23 15:48:04 +0100

    La til togruteforekomster

--- BrukerhistorieF.sql
+++ BrukerhistorieF.sql
@@ -2,9 +2,9 @@
 -- Det vil nå være mulig å opprette en kundeordre som kan kjøpe en eller flere billetter for en togruteforekomst.
 -- Det må selvfølgelig legges inn programmering for å sjekke om billetten er ledig og om start og sluttstasjonen man ønsker 
 -- faktisk er en del av togruteforekomsten. Dette gjøre i brukerhistorie g).
-INSERT INTO 'Togruteforekomst' VALUES('Nattog fra Trondheim til Bodø', '03/04/23')
-INSERT INTO 'Togruteforekomst' VALUES('Nattog fra Trondheim til Bodø', '04/04/23')
-INSERT INTO 'Togruteforekomst' VALUES('Dagtog fra Trondheim til Bodø', '03/04/23')
-INSERT INTO 'Togruteforekomst' VALUES('Dagtog fra Trondheim til Bodø', '04/04/23')
-INSERT INTO 'Togruteforekomst' VALUES('Morgentog fra Mo i Rana til Trondheim', '03/04/23')
-INSERT INTO 'Togruteforekomst' VALUES('Morgentog fra Mo i Rana til Trondheim', '04/04/23')
+INSERT INTO Togruteforekomst VALUES('Nattog fra Trondheim til Bodø', '03/04/23');
+INSERT INTO Togruteforekomst VALUES('Nattog fra Trondheim til Bodø', '04/04/23');
+INSERT INTO Togruteforekomst VALUES('Dagtog fra Trondheim til Bodø', '03/04/23');
+INSERT INTO Togruteforekomst VALUES('Dagtog fra Trondheim til Bodø', '04/04/23');
+INSERT INTO Togruteforekomst VALUES('Morgentog fra Mo i Rana til Trondheim', '03/04/23');
+INSERT INTO Togruteforekomst VALUES('Morgentog fra Mo i Rana til Trondheim', '04/04/23');
