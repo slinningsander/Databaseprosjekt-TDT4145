f09c4d1995daf7e678b9e867a5f1ec54e93a96b2
unknown <bcfogale@stud.ntnu.no>
2023-03-24 15:23:17 +0100

    La til billett i database

--- Insertions.py
+++ Insertions.py
@@ -2,11 +2,11 @@ import sqlite3
 con = sqlite3.connect('Jernbane.db')
 cursor = con.cursor()
 
-cursor.execute("""INSERT INTO 'Togruteforekomst' VALUES('Nattog fra Trondheim til Bodø', '04/04/23')""")
+cursor.execute("""INSERT INTO Billett VALUES (1, 1, NULL, 1, 1, "Steinkjer", "Fauske", "Dagtog fra Trondheim til Bodø", "03/04/23")""")
 
 con.commit()
 
-cursor.execute("""SELECT * FROM 'Togruteforekomst'""")
+cursor.execute("""SELECT * FROM 'Billett'""")
 
 print(cursor.fetchall())
 con.close()
